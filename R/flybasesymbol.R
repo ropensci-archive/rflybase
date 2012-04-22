54ad058367a38c3672d712a88641dd11b66230a3
Scott Chamberlain <myrmecocystus@gmail.com>
2012-04-22 08:43:33 -0500

    tidied code

--- R/flybasesymbol.R
+++ R/flybasesymbol.R
@@ -1,21 +1,21 @@
-# flybasesymbol.R
-
-flybasesymbol <- 
-# Args:
-#   symbol: flybase symbol (character)
-# Output: xml
-# Examples:
-#   flybasesymbol("dro5-PA")
-
-function(symbol = NA,
-  url = 'http://flybase.org/chadoxml/symbol/',
-  ..., 
-  curl = getCurlHandle() ) 
+#' Returns XML document of Flybase data.
+#' 
+#' @import RCurl XML
+#' @param symbol flybase symbol (character)
+#' @param url the Flybase base API url for the function (should be left to default)
+#' @param ... optional additional curl options (debugging tools mostly)
+#' @param curl If using in a loop, call getCurlHandle() first and pass
+#' the returned value in here (avoids unnecessary footprint)
+#' @export
+#' @examples \dontrun{  
+#' flybasesymbol("dro5-PA")
+#' }
+flybasesymbol <- function(symbol = NA, url = 'http://flybase.org/chadoxml/symbol/',
+  ..., curl = getCurlHandle() ) 
 {
   url2 <- paste(url, symbol, sep='')
-  cat(url2)
+  message(url2)
   tt <- getURL(url2, curl = curl)
   xmlParse(tt)
 }
-
 # http://flybase.org/chadoxml/symbol/dro5-PA
